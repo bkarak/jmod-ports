dadba62bf3b00c9ca2db29c8eac2dbed52b5e2ce
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2014-02-07 23:38:29 +0200

    updated relative path

--- jmod-sql-benchmark/jmod-ns-aware/org/jmod/SimpleSQLConfiguration.java
+++ jmod-sql-benchmark/jmod-ns-aware/org/jmod/SimpleSQLConfiguration.java
@@ -5,5 +5,5 @@ import org.jmod.dsl.sql.SQLConfiguration;
 
 public class SimpleSQLConfiguration extends SQLConfiguration {
     public boolean SQLMOD_NS_AWARE = true;
-    public String SQLMOD_NS_URI = "file:///Users/bkarak/devel/research/jmodv2/examples/jmod-ns-aware/schema.sql";
+    public String SQLMOD_NS_URI = "examples/jmod-ns-aware/schema.sql";
 }
