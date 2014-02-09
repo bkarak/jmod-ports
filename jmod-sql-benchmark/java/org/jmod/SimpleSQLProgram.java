bb76c176cb995de4974818f10d2fa7d320c83921
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2014-02-09 21:11:41 +0200

    fixed version of SimpleSQLProgram

--- jmod-sql-benchmark/java/org/jmod/SimpleSQLProgram.java
+++ jmod-sql-benchmark/java/org/jmod/SimpleSQLProgram.java
@@ -8,11 +8,13 @@ import java.util.Properties;
 public class SimpleSQLProgram {
 	public static void main(String[] args) {
 		final String jdbcUrl = "jdbc:mysql://127.0.0.1/test";
+        final String jdbcDriver = "";
 
 		try {
     		Properties connectionProps = new Properties();
     		connectionProps.put("user", "root");
     		connectionProps.put("password", "");
+            Class.forName(jdbcDriver);
     		Connection conn = DriverManager.getConnection(jdbcUrl, connectionProps);
     		Statement stmnt = conn.createStatement();
         	ResultSet rs = stmnt.executeQuery("select * from customer");
